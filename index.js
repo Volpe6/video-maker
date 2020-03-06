ab9c2324a1f5f9d064c1d841c6b97887aec86644
Volpe6 <baasch2014@gmail.com>
2020-03-06 13:40:47 -0300

    adicionado busca pelo termo e prefixo

---
+++ index.js
@@ -0,0 +1,28 @@
+const readline = require('readline-sync'); 
+
+function start() {
+    const content = {};
+
+    content.searchTerm = askAndReturnSearchTerm();
+    content.prefix     = askAndReturnPrefix();
+
+    function askAndReturnSearchTerm() {
+        //termo da busca na wikipedia
+        return readline.question('Type a Wikipedia search term: ');
+    }
+
+    function askAndReturnPrefix() {
+        //prefixos para uma pesquisa mais completa no google
+        const prefixes = ['Who is', 'What is', 'The history of'];
+        //indice do prefixo escolhido
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ');
+        //texto referente ao indice do prefixo
+        const selectedPrefixText  = prefixes[selectedPrefixIndex];
+
+        return selectedPrefixText;
+    }
+
+    console.log(content);
+}
+
+start();
